0e0ce16a58e83fea71cb51a0e35f564f665768ea
Geniorama <geniorama.info@gmail.com>
2018-07-30 18:37:07 -0500

    Seccion interna pensionados

--- js/app.js
+++ js/app.js
@@ -15,4 +15,19 @@ $(document).ready(function() {
           	loop: true,
 
           });
+
+	//NAV TABS
+
+	$('.nav-tabs .nav-item').click(function(e) {
+		e.preventDefault();
+
+		var service = $(this).attr('data-service');
+
+		$('.nav-tabs .nav-item').removeClass('tab_active');
+		$(this).addClass('tab_active');
+
+
+		$('.card.card_services').slideUp();
+		$('.card.card_services[id="'+service+'"]').slideDown('able');
+	})
 });
